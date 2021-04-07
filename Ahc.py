ad675300e218254358d7449a83cc52fdac404dfd
WINS Laboratory <cengwins@gmail.com>
2021-04-07 11:18:24 +0300

    Update Ahc.py

--- Ahc.py
+++ Ahc.py
@@ -1,3 +1,35 @@
+#!/usr/bin/env python
+""" Implements the AHC library.
+
+TODO: Longer description of this module to be written.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+__author__ = "One solo developer"
+__authors__ = ["Ertan Onur", "Berke Tezergil", "etc"]
+__contact__ = "eonur@ceng.metu.edu.tr"
+__copyright__ = "Copyright 2021, WINSLAB"
+__credits__ = ["Ertan Onur", "Berke Tezergil", "etc"]
+__date__ = "2021/04/07"
+__deprecated__ = False
+__email__ =  "eonur@ceng.metu.edu.tr"
+__license__ = "GPLv3"
+__maintainer__ = "developer"
+__status__ = "Production"
+__version__ = "0.0.1"
+
+
 import datetime
 import queue
 from enum import Enum
