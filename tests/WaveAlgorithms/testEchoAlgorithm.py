fb0425cbb22e9bc38f220b01be2ac564448f0070
kovanarastirmalabi <kovanswarmuav@gmail.com>
2021-05-26 02:28:38 +0300

    Implemented Tree and Echo Algorithms

---
+++ tests/WaveAlgorithms/testEchoAlgorithm.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import time
+import random
+
+sys.path.insert(0, os.getcwd())
+
+from Channels import Channel
+from WaveAlgorithms.EchoAlgorithm import *
+
+def main():
+
+  G = nx.random_geometric_graph(80, 0.5)
+  nx.draw(G, with_labels=True, font_weight='bold')
+  plt.draw()
+
+  topo = Topology()
+  topo.construct_from_graph(G, EchoNode, Channel)
+  # topo.start()
+  # time.sleep(3)
+  start = time.time()
+  startEchoAlgorithm(topo)
+  print(f"Start Time: {start}")
+  plt.show() 
+  while (True): pass
+
+if __name__ == "__main__":
+  main()
