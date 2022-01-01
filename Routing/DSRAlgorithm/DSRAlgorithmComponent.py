e14be9ff78cd3044376c185c9af92f7e768cfb19
Burak Sezgin <76393782+zerohoc@users.noreply.github.com>
2022-01-01 21:29:46 +0300

    Cache added

--- Routing/DSRAlgorithm/DSRAlgorithmComponent.py
+++ Routing/DSRAlgorithm/DSRAlgorithmComponent.py
@@ -1,4 +1,5 @@
 import enum
+import threading
 import time
 
 from Ahc import ComponentModel
@@ -18,6 +19,57 @@ class MessageTypes(enum.Enum):
     ROUTE_REQUEST = "ROUTE_REQUEST"
 
 
+class Cache:
+
+    def __init__(self):
+        self.lock = threading.RLock()
+        self.cache = {}
+
+    def has(self, key) -> bool:
+        with self.lock:
+
+            if key in self.cache:
+                return True
+
+            return False
+
+    def get_value(self, key):
+        with self.lock:
+
+            if self.has(key):
+                return None
+            else:
+                value = self.cache[key]
+                return value
+
+    def set_value(self, key, value):
+        with self.lock:
+
+            self.cache[key] = value
+
+    def delete_key(self, key):
+        with self.lock:
+
+            self.cache.pop(key, None)
+
+    def has_same_value(self, key, value) -> bool:
+        with self.lock:
+
+            if self.has(key):
+                return value == self.cache[key]
+            else:
+                return False
+
+    def delete_keys_with_link(self, link):
+        with self.lock:
+
+            for key in list(self.cache.keys()):
+                value = self.cache[key]
+                if value.count(link[0]) > 0 and value.count(link[1]) > 0:
+                    if value.index(link[0]) + 1 == value.index(link[1]):
+                        self.delete_key(key)
+
+
 class DSRAlgorithmComponent(ComponentModel):
 
     def __init__(self, component_name: str, componentinstancenumber: int):
@@ -25,23 +77,18 @@ class DSRAlgorithmComponent(ComponentModel):
         self.hop_limit = 9
         self.uid = 0
         self.trial_number = 3
-        self.route_cache = {}
-        self.route_req_cache = {}
+        self.route_cache = Cache()
+        self.route_request_table = Cache()
 
     def on_init(self, event_obj: Event):
         if self.get_component_id() == 0:
-            dst = len(ComponentRegistry.components) - 1
-            thread = Thread(target=self.start_data_sending, args=[dst, "Data send by componentinstancenumber == 0"])
+            last_components_id = len(ComponentRegistry.components) - 1
+            thread = Thread(target=self.start_data_sending,
+                            args=[last_components_id, "Data send by componentinstancenumber == 0"])
             thread.start()
 
     def is_destination(self, dst: int) -> bool:
         return dst == self.get_component_id()
-
-    def is_destination_in_cache(self, dst: int) -> bool:
-        if dst in self.route_cache:
-            return True
-
-        return False
 
     def is_link_up(self, next_component_id: int) -> bool:
         if next_component_id in self.connectors[ConnectorTypes.PEER]:
@@ -49,11 +96,13 @@ class DSRAlgorithmComponent(ComponentModel):
         return False
 
     def is_route_request_seen_before(self, src: int, uid: int) -> bool:
-        if src in self.route_req_cache:
-            if self.route_req_cache[src] >= uid:
+
+        if self.route_request_table.has(src):
+            latest_uid = self.route_request_table.get_value(src)
+            if latest_uid >= uid:
                 return True
 
-        self.route_req_cache[src] = uid
+        self.route_request_table.set_value(src, uid)
         return False
 
     def is_source(self, src: int) -> bool:
@@ -90,9 +139,6 @@ class DSRAlgorithmComponent(ComponentModel):
             print("route = " + str_route)
             return None
 
-    def get_route_from_cache(self, dst: int) -> list:
-        return self.route_cache[dst]
-
     def create_event(self, message_type, src: int, dst: int, route: list, data=None) -> Event:
         message_header = GenericMessageHeader(message_type, src, dst)
         message_payload = [route, data]
@@ -106,6 +152,20 @@ class DSRAlgorithmComponent(ComponentModel):
     def process_message(self, data: str) -> None:
         print(f"I am {self.componentname} {self.componentinstancenumber}, data={data}\n")
 
+    def start_data_sending(self, dst: int, data) -> None:
+
+        for i in range(self.trial_number):
+
+            if self.route_cache.has(dst):
+                src = self.get_component_id()
+                route = self.route_cache.get_value(dst)
+
+                self.transmit_route_forwarding(src, dst, route, data)
+                return
+
+            # waits in start_route_discovery
+            self.start_route_discovery(dst)
+
     def start_route_discovery(self, dst: int) -> None:
         src = self.get_component_id()
         route = [src]
@@ -116,20 +176,6 @@ class DSRAlgorithmComponent(ComponentModel):
 
     def start_route_maintenance(self, dst: int) -> None:
         self.start_route_discovery(dst)
-
-    def start_data_sending(self, dst: int, data) -> None:
-
-        for i in range(self.trial_number):
-
-            if self.is_destination_in_cache(dst):
-                src = self.get_component_id()
-                route = self.get_route_from_cache(dst)
-
-                self.transmit_route_forwarding(src, dst, route, data)
-                return
-
-            # waits in start_route_discovery
-            self.start_route_discovery(dst)
 
     def wait_for_route_reply(self, dst: int) -> None:
 
@@ -143,7 +189,7 @@ class DSRAlgorithmComponent(ComponentModel):
 
         # overflow check in time
         while end_time_in_ms > self.get_current_time_in_ms():
-            if self.is_destination_in_cache(dst):
+            if self.route_cache.has(dst):
                 return
 
             time.sleep(sleep_period_in_sec)
@@ -177,14 +223,30 @@ class DSRAlgorithmComponent(ComponentModel):
 
     def receive_route_error(self, src: int, dst: int, route: list, broken_link: list) -> None:
 
-        # delete from cache
+        self.route_cache.delete_keys_with_link(broken_link)
+
         if self.is_destination(dst):
             self.start_route_maintenance(dst)
         else:
             self.transmit_route_error(src, dst, route, broken_link)
 
     def receive_route_reply(self, src: int, dst: int, route: list) -> None:
-        # cache
+        try:
+            index_of_current_component = route.index(self.get_component_id())
+            new_route = route[index_of_current_component:-1]
+
+            if not self.route_cache.has(src):
+                self.route_cache.set_value(src, new_route)
+            elif len(new_route) < len(self.route_cache.get_value(src)):
+                self.route_cache.set_value(src, new_route)
+
+        except ValueError:
+            print("[DSRAlgorithmComponent:get_next_component_id][Exception] ValueError")
+            print("component_id = " + str(self.get_component_id()))
+            str_route = ' '.join(map(str, route))
+            print("route = " + str_route)
+            return None
+
         if not self.is_destination(dst):
             self.transmit_route_reply(src, dst, route)
 
@@ -196,12 +258,26 @@ class DSRAlgorithmComponent(ComponentModel):
             route.append(self.get_component_id())
             new_src = dst
             new_dst = src
-            # add to cache
+
+            if not self.route_cache.has(src):
+                self.route_cache.set_value(src, route[::-1])
+            elif len(route) < len(self.route_cache.get_value(src)):
+                self.route_cache.set_value(src, route[::-1])
+
             self.transmit_route_reply(new_src, new_dst, route)
 
-        elif self.is_destination_in_cache(dst):
-            # add to cache
-            rest_of_the_route = self.get_route_from_cache(dst)
+        elif self.route_cache.has(dst):
+
+            if not self.route_cache.has(src):
+                new_route = route
+                new_route.append(self.get_component_id())
+                self.route_cache.set_value(src, new_route[::-1])
+            elif len(route) + 1 < len(self.route_cache.get_value(src)):
+                new_route = route
+                new_route.append(self.get_component_id())
+                self.route_cache.set_value(src, new_route[::-1])
+
+            rest_of_the_route = self.route_cache.get_value(dst)
             route.append(rest_of_the_route)
             new_src = dst
             new_dst = src
@@ -214,7 +290,12 @@ class DSRAlgorithmComponent(ComponentModel):
         else:
             if len(route) < self.hop_limit:
                 route.append(self.get_component_id())
-                # add to cache
+
+                if not self.route_cache.has(src):
+                    self.route_cache.set_value(src, route[::-1])
+                elif len(route[::-1]) < len(self.route_cache.get_value(src)):
+                    self.route_cache.set_value(src, route[::-1])
+
                 self.transmit_route_request(src, dst, route, uid)
 
     def transmit_route_forwarding(self, src: int, dst: int, route: list, data) -> None:
@@ -276,3 +357,4 @@ class DSRAlgorithmComponent(ComponentModel):
 
         event = self.create_event(MessageTypes.ROUTE_REQUEST, src, dst, route, uid)
         self.send_peer(event)
+
