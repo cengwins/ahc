901ad2942b9df559eb72c91d19ae569437878ee7
DorukGercel <dorukgercel@hotmail.com>
2022-01-06 22:19:35 +0300

    Create ZKP Hamiltonian Cycle test

---
+++ tests/CryptoProtocols/testZkpHamiltonianCycle.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from Ahc import Topology
+from CryptoProtocols.ZkpHamiltonianCycle import PeggyAdHocNode, VictorAdHocNode
+from Channels.Channels import P2PFIFOPerfectChannel
+
+sys.path.insert(0, os.getcwd())
+
+
+def main():
+    topo = Topology()
+    topo.construct_sender_receiver(PeggyAdHocNode, VictorAdHocNode, P2PFIFOPerfectChannel)
+
+    topo.start()
+    while True:
+        continue
+
+
+if __name__ == "__main__":
+    main()
