36422006c4cd2cbd46c76cecf40c3eb2387bf3e2
cengwins <eronur@metu.edu.tr>
2022-01-13 19:26:00 +0300

    github-PYPI integration workflow is added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='adhocomputing',
-    version='0.0.3',
+    version='0.0.4',
 
     url='https://github.com/cengwins/ahc',
     author='Ertan Onur',
