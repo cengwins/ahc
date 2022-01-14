de071f78650e6138c46cdf5b2e6d132cb9a46436
cengwins <cengwins@gmail.com>
2022-01-14 17:46:27 +0300

    GSR is added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="adhoccomputing",
-    version="0.0.15",
+    version="0.0.16",
     author="Ertan Onur",
     author_email="eronur@metu.edu.tr",
     description="A small example package",
