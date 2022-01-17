dfeda6475da4eb29f134cd84d32086f88c60dbb6
cengwins <cengwins@gmail.com>
2022-01-17 17:46:58 +0300

    MentalPoker is added to Security

--- ahc/Security/MentalPoker.py
+++ ahc/Security/MentalPoker.py
@@ -1,5 +1,5 @@
-from Ahc import ComponentModel, Event, EventTypes, GenericMessageHeader, GenericMessage, Topology, ConnectorTypes
-from Channels.Channels import Channel
+from ahc.Ahc import ComponentModel, Event, EventTypes, GenericMessageHeader, GenericMessage, Topology, ConnectorTypes
+from ahc.Channels.Channels import Channel
 
 from time import time, sleep
 
@@ -251,34 +251,3 @@ class Carol(ComponentModel):
                 self.send_down(event)
 
 #Construction of experiment with topology
-
-def main():
-    topo = Topology()
-    alice = Alice("Alice",0)
-    bob = Bob("Bob",1)
-    carol = Carol("Carol",2)
-    channel1 = Channel("Channel1", "0-1")
-    channel2 = Channel("Channel2", "0-2")
-    channel3 = Channel("Channel3", "1-2")
-    graph = networkx.Graph()
-    graph.add_nodes_from([0, 1, 2])
-    graph.add_edges_from([(0, 1),(0, 2),(1, 2)])
-    topo.G = graph
-    topo.nodes[alice.componentinstancenumber] = alice
-    topo.nodes[bob.componentinstancenumber] = bob
-    topo.nodes[carol.componentinstancenumber] = carol
-    topo.channels[channel1.componentinstancenumber] = channel1
-    topo.channels[channel2.componentinstancenumber] = channel2
-    topo.channels[channel3.componentinstancenumber] = channel3
-    alice.connect_me_to_channel(ConnectorTypes.DOWN, channel1)
-    alice.connect_me_to_channel(ConnectorTypes.DOWN, channel2)
-    bob.connect_me_to_channel(ConnectorTypes.DOWN, channel1)
-    bob.connect_me_to_channel(ConnectorTypes.DOWN, channel3)
-    carol.connect_me_to_channel(ConnectorTypes.DOWN, channel2)
-    carol.connect_me_to_channel(ConnectorTypes.DOWN, channel3)
-    topo.start()
-    sleep(10)
-
-
-if __name__ == "__main__":
-    main()
