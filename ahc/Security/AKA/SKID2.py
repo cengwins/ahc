b4755141202ac8b90beb0a9f4a4a53ddda15b28d
cengwins <cengwins@gmail.com>
2022-01-18 11:53:31 +0300

    SKID2 is added

--- ahc/Security/AKA/SKID2.py
+++ ahc/Security/AKA/SKID2.py
@@ -16,12 +16,12 @@ sys.path.insert(1, (os.path.join(os.getcwd(), "../")))
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from Ahc import ComponentModel, Event, ConnectorTypes, Topology
-from Ahc import ComponentRegistry
-from Ahc import GenericMessagePayload, GenericMessageHeader, GenericMessage, EventTypes
-from Channels.Channels import P2PFIFOPerfectChannel
-from LinkLayers.GenericLinkLayer import LinkLayer
-from NetworkLayers.AllSeeingEyeNetworkLayer import AllSeingEyeNetworkLayer
+from ahc.Ahc import ComponentModel, Event, ConnectorTypes, Topology
+from ahc.Ahc import ComponentRegistry
+from ahc.Ahc import GenericMessagePayload, GenericMessageHeader, GenericMessage, EventTypes
+from ahc.Channels.Channels import P2PFIFOPerfectChannel
+from ahc.LinkLayers.GenericLinkLayer import LinkLayer
+from ahc.Routing.AllSeeingEyeNetworkLayer import AllSeingEyeNetworkLayer
 
 registry = ComponentRegistry()
 
@@ -360,9 +360,9 @@ def main():
     topo.construct_sender_receiver(Alice, Bob, P2PFIFOPerfectChannel)
     topo.start()
 
-    while(True):
-        pass
-
+    #while(True):
+    #    pass
+    time.sleep(5)
 
 if __name__ == "__main__":
     main()
