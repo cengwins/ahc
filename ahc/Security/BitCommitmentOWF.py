ffc0df50ebf802e87c85acf8709375b004463402
cengwins <cengwins@gmail.com>
2022-01-18 11:38:44 +0300

    Bit Commitment using One-way Functions is added

--- ahc/Security/BitCommitmentOWF.py
+++ ahc/Security/BitCommitmentOWF.py
@@ -3,11 +3,11 @@ from time import time, sleep
 
 import networkx
 
-from Ahc import ComponentModel, Event, EventTypes, GenericMessageHeader, GenericMessage, Topology, ConnectorTypes
+from ahc.Ahc import ComponentModel, Event, EventTypes, GenericMessageHeader, GenericMessage, Topology, ConnectorTypes
 import hashlib
 from secrets import SystemRandom
 
-from Channels.Channels import Channel
+from ahc.Channels.Channels import Channel
 
 BIT_LENGTH = 1024
 BYTE_LENGTH = BIT_LENGTH // 8
@@ -117,24 +117,3 @@ class Bob(ComponentModel):
 Topology is created here.
 """
 
-
-def main():
-    topology = Topology()
-    sender = Alice("Alice", 0)
-    receiver = Bob("Bob", 1)
-    channel = Channel("Channel", "0-1")
-    graph = networkx.Graph()
-    graph.add_nodes_from([0, 1])
-    graph.add_edges_from([(0, 1)])
-    topology.G = graph
-    topology.nodes[sender.componentinstancenumber] = sender
-    topology.nodes[receiver.componentinstancenumber] = receiver
-    topology.channels[channel.componentinstancenumber] = channel
-    sender.connect_me_to_channel(ConnectorTypes.DOWN, channel)
-    receiver.connect_me_to_channel(ConnectorTypes.DOWN, channel)
-    topology.start()
-    sleep(10)
-
-
-if __name__ == "__main__":
-    main()
