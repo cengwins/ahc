89a9d6c788826900c677d1b5ec8725874e7e9111
saidu <saidu@winscenter.ceng.metu.edu.tr>
2022-02-22 13:05:18 +0300

    Added SGLC implementation and test

---
+++ Clocks/compression.py
@@ -0,0 +1,298 @@
+import math
+import operator
+import networkx as nx
+import matplotlib.pyplot as plt
+from scipy.misc import * 
+import threading
+import numpy as np
+
+
+mutex1 = threading.Lock()
+mutex2 = threading.Lock()
+
+def compareSizes(n, m, bits):
+   
+    AdjLst = (n * 32) + (m * 32)
+    AdjMat = n * n 
+    sizeEncode = n * (n - 1)
+    sizeEncodeEdge = math.log(comb(n * (n - 1), m), 2)
+
+    return [AdjLst, AdjMat, sizeEncode, sizeEncodeEdge]
+
+def comb2(n):
+    return n * (n - 1) / 2.0
+
+def perm(n, k): 
+    p = 1
+
+    for i in range(n, (n-k), -1): 
+        p = p * i
+
+    return p 
+
+def comb(n, k):
+    comb = perm(n, k) // math.factorial(k)
+    return comb
+
+def H(i):
+    return 1 + math.ceil( -1/2 + math.sqrt(1/4 + i))
+
+
+def mynewtonana(f, x0, c, g, m, epsilon, max_iter):
+    '''Approximate solution of f(x)=0 by Newton's method using analytical first derivative. '''
+
+    # Input - f: the objection/cost function 
+    #       - x0: initial approximation to a zero of f
+    #       - c: current edge label, used for finding the derivate 
+    #       - g: the integer used to represent the graph, use for overcoming an unexpected occurence
+    #       - delta: tolerance for f(xn), f(xn) < delta
+    #       - epsilon: one of the stopping criteria, the bound for |xn1 - xn| < epsilon
+    #       - max_iter: maximum number of iterations
+    # Output - xn; the Newton-Raphson approximation to the zero
+
+    
+    xn = x0
+    for n in range(0, max_iter):
+        if ( xn >= c):
+            fxn = f(xn)
+
+            if fxn == 0 : 
+                #print(f"ending after {n} iterations fxn = 0 sending {xn}")
+                return xn
+
+            Dfxn = Jprime(xn, c) 
+  
+            if math.fabs(fxn) < epsilon: 
+                #print(f"ending after {n} iterations fxn = {fxn} sending {xn}")
+                return xn
+
+
+            if Dfxn == 0:
+                print('Zero derivative. No solution found.')
+                return xn 
+         
+            xn1 = xn - f(xn)/Dfxn
+            
+            err = math.fabs(xn1 - xn)
+
+            relerror = (2 * err) / (math.fabs(xn1) + epsilon)
+            xn = xn1   
+
+            if (relerror < epsilon ):
+                #print(f"ending after {n} iterations relative error sending {xn}")
+                return xn1
+
+            # Solution to an unpredictable problem
+            if (math.floor(xn) + 2 == g+1):
+                return math.fabs(xn+1)
+        else:
+            xn = c
+
+    return math.fabs(xn)
+
+
+
+
+############################################### BOTH N AND M UNKNOWN #################################################################
+
+def encode(graph):
+    bi = {}
+    i = 0 
+
+    for k, v in graph.adjacency(): 
+        for m in v: 
+            u, v = int(k), int(m)
+            i = (2 * u + (v - 1)*(v - 2)) if u < v else (2 * v + u * (u - 3) + 1)
+            bi[int(i)] = 1
+
+    return bi
+
+
+def decode(enc): 
+    graph = nx.DiGraph()
+
+    for i in enc:
+        bi = enc[i]
+        if (i % 2) != 0:
+            u = int(H(i))
+            v = int(((i + 1) / 2) - comb2(u-1))
+            graph.add_edge(u,v)
+        else: 
+            v = int(H(i))
+            u = int((i/2) - comb2(v-1))
+            graph.add_edge(u,v)
+
+    return graph 
+
+############################################### ONLY N KNOWN #################################################################
+
+def encode_n(graph, n):
+    bi = [0 for j in range(n * (n - 1) + 1)]
+
+    def C(u, v, n):
+        return (u / 2.0) * (2 * n - 1 - u) + v - n
+
+    def F(u, v, n, a):
+        return a if u < v else a + comb2(n)
+
+    i = 0
+
+    for k, v in graph.adjacency():
+        for m in v:
+            u, v = int(k), int(m)
+            i = C(u, v, n) if u < v else C(v, u, n) + comb2(n)
+            bi[int(i)] = 1
+
+    return bi
+
+
+def decode_n(enc, n):
+    graph = nx.DiGraph()
+
+    for i in range(1, len(enc)): 
+        bi = enc[i]
+        if bi: 
+            j = i if i <= comb2(n) else i - comb2(n)
+            t = n - (1.0 / 2)
+
+            u = int(math.ceil(t - math.sqrt(t * t - 2 * j)))
+            v = int(j + n + u * ((u + 1) / 2.0 - n))
+
+            if i <= comb2(n): 
+                graph.add_edge(u, v)
+            else: 
+                graph.add_edge(v, u)
+
+    return graph
+
+
+############################################### N AND M KNOWN #################################################################
+
+class Link: 
+    def __init__(self, u, v): 
+        self.src = u
+        self.dest = v
+        self.num = (2 * u) + (v - 1) * (v - 2) if u < v else (2 * v) + u *(u - 3) + 1
+
+
+def J(x, c, g):
+
+    if g <= 1: 
+        g = 1
+    tot = 0 
+    #cons = (1.0 / math.log10(g))
+
+    for p in range(1, c + 1): 
+        tot += math.log10((x-c)/(p * 1.0) + 1)
+    
+    #return math.fabs(cons*tot-1)
+    # Alternatively
+    result = tot - math.log10(g)
+
+    return result
+
+
+def Jprime(x, c): 
+    tot = np.float64(0.0)
+    ln = np.float64(1.0) / math.log(10)
+
+    for p in range(0, c): 
+        tot += np.float64(1.0) / np.float64(x-p)
+
+    return tot*ln
+
+# Check again doesn't seem to work
+def J2(x,c,g ):
+    return comb(int(x), int(c)) - g
+
+
+def encode_nm(graph): 
+    global mutex1
+    mutex1.acquire()
+    m = 0
+    j = 1
+    tot = 0 
+    links = []
+
+    # Number of links 
+    for k, v in graph.adjacency(): 
+        m += len(v)
+
+    # Encode links 
+    for k, v in graph.adjacency(): 
+        for r in v: 
+            u, v = int(k), int(r)
+            links.append(Link(u, v))
+
+    # Sort links by i_c
+    links.sort(key=operator.attrgetter('num'))
+
+
+    for l in links: 
+        tot += int(math.pow(-1, m - j)) * (comb(l.num, j)-1)
+        j += 1
+
+    mutex1.release()
+    return  m, tot
+
+
+def decode_nm(g, m): 
+    global mutex2
+    mutex2.acquire()
+    graph = nx.DiGraph()
+    x = m
+
+    for c in range(m, 0, -1): 
+        y = lambda x: J(x, c, g)        
+        result = mynewtonana(y, x, c, g, m, 10e-11, 12)
+        decimal = result - int(result)
+        
+        if (round(decimal, 5) == 0.00000): 
+            # Reveluate with less tolerance
+            xn = mynewtonana(y, x, c, g, m, 10e-6, 12)
+            result = xn
+
+        x = result
+        i = math.floor(x) + 1
+
+        if (i % 2) != 0:
+
+            u = int(H(i))
+            v = int(((i + 1) / 2) - comb2(u-1))
+            graph.add_edge(u,v)
+        else: 
+            v = int(H(i))
+            u = int((i/2) - comb2(v-1))
+            graph.add_edge(u,v)
+      
+        g = comb(i, c) - g -1
+        
+    mutex2.release()
+    return graph
+
+
+############################################################# MAIN ###############################################
+
+
+if __name__ == "__main__":
+
+    # A example to test the coding and decoding algorithms 
+    G = nx.DiGraph()
+    G.add_edges_from(
+        [(1, 5), (2, 4), (4, 3), (4 , 5), (5, 6), (6, 12), 
+         (11,12), (13, 21), (21, 22), (22, 23), (25, 26), 
+         (23, 24), (26, 23), (27, 28), (28, 29), (29, 30), 
+         (33, 34), (34, 35), (8, 9), (36, 9), (9, 10) ])
+
+    # Ecnode the graph G
+    m, tot = encode_nm(G)
+
+    # decode the graph
+    dec = decode_nm(tot,m)
+    
+    # Print out the decoded graph
+    for k, v in dec.adjacency():
+        print(f"{k}: {dec[k]}")
+
+
+
---
+++ Clocks/testSGLC.py
@@ -0,0 +1,105 @@
+import random
+import time
+from datetime import datetime
+from enum import Enum
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+from Ahc import ComponentModel, Event, ConnectorTypes, Topology
+from Ahc import ComponentRegistry
+from Ahc import GenericMessagePayload, GenericMessageHeader, GenericMessage, EventTypes, MessageDestinationIdentifiers
+
+from PhysicalLayers.Channels import P2PFIFOPerfectChannel
+from LinkLayers.GenericLinkLayer import LinkLayer
+from NetworkLayers.AllSeeingEyeNetworkLayer import AllSeingEyeNetworkLayer
+
+from Clocks.LogicalClocks import LogicalClock, VectorClock, SuccinctClock3
+
+registry = ComponentRegistry()
+
+class ApplicationLayerMessageTypes(Enum): 
+  pass
+
+class ApplicationLayerMessageHeader(GenericMessageHeader):
+    pass 
+
+class ApplicationLayerMessagePayload(GenericMessagePayload):
+  pass 
+
+
+
+class ApplicationLayerComponent(ComponentModel):
+
+  def on_init(self, eventobj: Event):
+    #print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
+    if self.componentinstancenumber == 0:
+      self.send_down(Event(self, EventTypes.MFRT, None))
+
+  def on_message_from_bottom(self, eventobj: Event):
+    self.send_down(Event(self, EventTypes.MFRT, None))  
+
+  def __init__(self, componentname, componentinstancenumber): 
+    super().__init__(componentname, componentinstancenumber)
+ 
+
+class AdHocNode(ComponentModel):
+
+  def on_init(self, eventobj: Event):
+    #print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
+    pass
+  def on_message_from_top(self, eventobj: Event):
+    self.send_down(Event(self, EventTypes.MFRT, eventobj.eventcontent))
+
+  def on_message_from_bottom(self, eventobj: Event):
+    self.send_up(Event(self, EventTypes.MFRB, eventobj.eventcontent))
+
+  def __init__(self, componentname, componentid):
+    super().__init__(componentname, componentid)
+
+    # SUBCOMPONENTSc
+    self.appllayer = ApplicationLayerComponent("ApplicationLayer", componentid)
+    #self.middleware = LogicalClock("LogicalClock", componentid)
+
+    self.middleware = SuccinctClock3("succicnt", componentid)
+    self.netlayer = AllSeingEyeNetworkLayer("NetworkLayer", componentid)
+    self.linklayer = LinkLayer("LinkLayer", componentid)
+    # self.failuredetect = GenericFailureDetector("FailureDetector", componentid)
+
+    # CONNECTIONS AMONG SUBCOMPONENTS
+    self.appllayer.connect_me_to_component(ConnectorTypes.DOWN, self.middleware)
+    self.middleware.connect_me_to_component(ConnectorTypes.UP, self.appllayer)
+    self.middleware.connect_me_to_component(ConnectorTypes.DOWN, self.netlayer)
+    self.netlayer.connect_me_to_component(ConnectorTypes.UP, self.middleware)
+    self.netlayer.connect_me_to_component(ConnectorTypes.DOWN, self.linklayer)
+    self.linklayer.connect_me_to_component(ConnectorTypes.UP, self.netlayer)
+
+    # Connect the bottom component to the composite component....
+    self.linklayer.connect_me_to_component(ConnectorTypes.DOWN, self)
+    self.connect_me_to_component(ConnectorTypes.UP, self.linklayer)
+
+
+
+    
+def main():
+
+  G = nx.Graph()
+
+  # Full mesh topology with 4 processes
+  G.add_edges_from([(0, 1), (0, 2), (0, 3), 
+                    (1, 2), (1, 3),
+                    (2, 3)])  
+
+  topo = Topology()
+  topo.construct_from_graph(G, AdHocNode, P2PFIFOPerfectChannel)
+
+  maxEvent = input("Please enter the maximum number of events you want to simulate at each process: ")
+  maxEvent = int(maxEvent)
+
+  for node in topo.nodes: 
+    topo.nodes[node].middleware.setMaxEvents(10)
+
+  topo.start()
+  time.sleep(30)
+if __name__ == "__main__":
+  main()
