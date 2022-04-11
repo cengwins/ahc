6bd483eef6cafb86fcf7da0bf2a78ab72699f2bb
ufukyagmur <uyagmur123@gmail.com>
2022-04-12 01:41:39 +0300

    fixing setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
   # TODO: Fix the configs in this part
-  name='ahc',
-  version='0.1.0',    
+  name='adhoccomputing',
+  version='2.0.1',    
   description="Ad Hoc Computing Library",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url='https://gitlab.ceng.metu.edu.tr/group-17/ahc-library',
+  url='https://github.com/cengwins/ahc',
   author='CengWins',
-  author_email='eronur@metu.edu.tr',
+  author_email='ahc@ceng.metu.edu.tr',
   license='BSD 2-clause',
-  packages=['ahc'],
+  packages=['adhoccomputing'],
   install_requires=[
     'matplotlib',
     'mnist',
@@ -33,6 +33,7 @@ setup(
   ],
   project_urls={
         "Bug Tracker": "https://github.com/cengwins/ahc/issues",
+        "Simulator Website": "https://ahc.ceng.metu.edu.tr"
   },
   classifiers=[
         "Programming Language :: Python :: 3",
