3d9eb21b6822675169bbacc12b302ff3033b95d7
Ozan Akın <ozn.akn@gmail.com>
2022-04-13 16:22:57 +0300

    bump version to 2.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
   # TODO: Fix the configs in this part
   name='adhoccomputing',
-  version='2.0.3',    
+  version='2.0.4',
   description="Ad Hoc Computing Library",
   long_description=long_description,
   long_description_content_type="text/markdown",
