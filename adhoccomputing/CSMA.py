42477e2cc7c4815c639396fce460b2608c09342d
ufukyagmur <uyagmur123@gmail.com>
2022-04-13 22:18:14 +0300

    fixed import in CSMA

--- adhoccomputing/CSMA.py
+++ adhoccomputing/CSMA.py
@@ -1,5 +1,5 @@
-from ...Generics import Event
-from ...Definitions import EventTypes, ComponentConfigurationParameters
+from .Generics import Event
+from .Definitions import EventTypes, ComponentConfigurationParameters
 from .GenericMAC import GenericMac, GenericMacEventTypes
 import time, random, math
 
