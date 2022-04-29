76e2f8b8116d1280a046e9c028b76c2a29e1526c
Ertan Onur <eonur@Ertans-MacBook-Pro.local>
2022-04-29 16:32:27 +0300

    Refactored phy

--- adhoccomputing/Networking/PhysicalLayer/UsrpB210OfdmFlexFramePhy.py
+++ adhoccomputing/Networking/PhysicalLayer/UsrpB210OfdmFlexFramePhy.py
@@ -110,7 +110,7 @@ class UsrpB210OfdmFlexFramePhy(FrameHandlerBase):
     # Callbacks have to be outside since the c library does not like "self"
     # Because of this reason will use userdata to get access back to the framer object 
 
-    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None, framers=None):
+    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
         self.framers = framers
-        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology, framers)
+        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology, self.framers)
         
