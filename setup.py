258cd2f505c277e3b81eead1493a7b1a028a18c9
ufukyagmur <uyagmur123@gmail.com>
2022-05-06 19:21:16 +0300

    version bump to 2.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
   # TODO: Fix the configs in this part
   name='adhoccomputing',
-  version='2.1.0',
+  version='2.1.1',
   description="Ad Hoc Computing Library",
   long_description=long_description,
   long_description_content_type="text/markdown",
