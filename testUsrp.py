ebfae435be0e1de36ca107e0187bb0494adbd19c
Ertan Onur <eonur@ertano.ceng.metu.edu.tr>
2022-06-02 12:37:10 +0300

    testUsrp uses ping pong

--- testUsrp.py
+++ testUsrp.py
@@ -11,62 +11,8 @@ from adhoccomputing.Generics import *
 from adhoccomputing.Experimentation.Topology import Topology
 from adhoccomputing.Networking.PhysicalLayer.UsrpB210OfdmFlexFramePhy import  UsrpB210OfdmFlexFramePhy
 from adhoccomputing.Networking.MacProtocol.CSMA import MacCsmaPPersistent, MacCsmaPPersistentConfigurationParameters
+from adhoccomputing.Networking.ApplicationLayer.PingPongApplicationLayer import *
 
-
-# define your own message types
-class ApplicationLayerMessageTypes(Enum):
-    BROADCAST = "BROADCAST"
-
-
-# define your own message header structure
-class ApplicationLayerMessageHeader(GenericMessageHeader):
-    pass
-
-
-class UsrpApplicationLayerEventTypes(Enum):
-    STARTBROADCAST = "startbroadcast"
-
-
-class UsrpApplicationLayer(GenericModel):
-    def on_init(self, eventobj: Event):
-        self.counter = 0
-    
-    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
-        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
-        self.eventhandlers[UsrpApplicationLayerEventTypes.STARTBROADCAST] = self.on_startbroadcast
-
-    def on_message_from_top(self, eventobj: Event):
-    # print(f"I am {self.componentname}.{self.componentinstancenumber},sending down eventcontent={eventobj.eventcontent}\n")
-        self.send_down(Event(self, EventTypes.MFRT, eventobj.eventcontent))
-    
-    def on_message_from_bottom(self, eventobj: Event):
-        evt = Event(self, EventTypes.MFRT, eventobj.eventcontent)
-        logger.applog(f"I am Node.{self.componentinstancenumber}, received from Node.{eventobj.eventcontent.header.messagefrom} a message: {eventobj.eventcontent.header.sequencenumber}")    
-        if self.componentinstancenumber == 1:
-            evt.eventcontent.header.messageto = 0
-            evt.eventcontent.header.messagefrom = 1
-        else:
-            evt.eventcontent.header.messageto = 1
-            evt.eventcontent.header.messagefrom = 0
-        evt.eventcontent.payload = eventobj.eventcontent.payload
-        evt.eventcontent.header.sequencenumber = eventobj.eventcontent.header.sequencenumber + 1
-        #print(f"I am {self.componentname}.{self.componentinstancenumber}, sending down eventcontent={eventobj.eventcontent.payload}\n")
-        self.send_down(evt)  # PINGPONG
-    
-    def on_startbroadcast(self, eventobj: Event):
-        if self.componentinstancenumber == 1:
-            hdr = ApplicationLayerMessageHeader(ApplicationLayerMessageTypes.BROADCAST, 1, 0)
-        else:
-            hdr = ApplicationLayerMessageHeader(ApplicationLayerMessageTypes.BROADCAST, 0, 1)
-        self.counter = self.counter + 1
-        hdr.sequencenumber = 1
-        payload = "BMSG-"*200 + str(self.counter)
-        broadcastmessage = GenericMessage(hdr, payload)
-        evt = Event(self, EventTypes.MFRT, broadcastmessage)
-        # time.sleep(3)
-        self.send_down(evt)
-        #print("Starting broadcast")
-    
          
 class UsrpNode(GenericModel):
     counter = 0
@@ -84,7 +30,8 @@ class UsrpNode(GenericModel):
         sdrconfig = SDRConfiguration(freq =2484000000.0, bandwidth = 20000000, chan = 0, hw_tx_gain = 76, hw_rx_gain = 20, sw_tx_gain = -12.0)
         bladerfconfig = SDRConfiguration(freq =900000000.0, bandwidth = 250000, chan = 0, hw_tx_gain = 50.0, hw_rx_gain = 20.0, sw_tx_gain = -12.0)
         
-        self.appl = UsrpApplicationLayer("UsrpApplicationLayer", componentinstancenumber, topology=topology)
+       
+        self.appl = PingPongApplicationLayer("PingPongApplicationLayer", componentinstancenumber, topology=topology)
         self.phy = UsrpB210OfdmFlexFramePhy("UsrpB210OfdmFlexFramePhy", componentinstancenumber, usrpconfig=sdrconfig, topology=topology)
         print(self.phy.sdrdev)
         self.mac = MacCsmaPPersistent("MacCsmaPPersistent", componentinstancenumber,  configurationparameters=macconfig, sdr=self.phy.sdrdev,topology=topology)
@@ -114,7 +61,7 @@ def main():
     topo = Topology()
 # Note that the topology has to specific: usrp winslab_b210_0 is run by instance 0 of the component
 # Therefore, the usrps have to have names winslab_b210_x where x \in (0 to nodecount-1)
-    topo.construct_winslab_topology_without_channels(2, UsrpNode)
+    topo.construct_winslab_topology_without_channels(1, UsrpNode)
   # topo.construct_winslab_topology_with_channels(2, UsrpNode, FIFOBroadcastPerfectChannel)
   
   # time.sleep(1)
@@ -125,7 +72,7 @@ def main():
     while(i < 100):
         #topo.nodes[1].appl.send_self(Event(topo.nodes[1], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
         #time.sleep(0.1)
-        topo.nodes[0].appl.send_self(Event(topo.nodes[0], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
+        topo.nodes[0].appl.send_self(Event(topo.nodes[0], PingPongApplicationLayerEventTypes.STARTBROADCAST, None))
         time.sleep(0.1)
         i = i + 1
     topo.exit()
