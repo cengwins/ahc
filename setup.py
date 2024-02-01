7930b5d5f8d92f6866b475914bdaaf6bd2b29ee0
arleontr <ertan.onur@arleon.com.tr>
2024-02-01 17:53:11 +0300

    Restructured Text Documentation Started

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-
 setup(
   # TODO: Fix the configs in this part
   name='adhoccomputing',
